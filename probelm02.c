66f989e7be5af865c24bd004bba4253b268ec62d
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:02:57 +0500

    Update probelm02.c

--- probelm02.c
+++ probelm02.c
@@ -1,1 +1,6 @@
+/* 
+A cinema charges ticket prices based on age: Age<12 → 200, Age 12-18 → 300, Age 18–60 → 500, Age>60 → 250, 
+Write a program to calculate ticket price based on age entered by the user.
+*/
+
 
