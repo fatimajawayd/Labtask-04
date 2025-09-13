8c01dc1512eb912ac4b02674f3525a7d9c36b598
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:07:38 +0500

    Update problem06.c

--- problem06.c
+++ problem06.c
@@ -1,1 +1,21 @@
+/* Q:06
+A program asks the user for their age. If they are 18 or older, print "Eligible to Vote",otherwise print "Not Eligible".
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("You are eligible to vote\n");
+    else
+        printf("You are not egligible to vote\n");
+        
+    return 0;    
+}
 
