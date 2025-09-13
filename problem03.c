d495920223cb561c3e34b3f8c76fa6f836e83178
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:05:17 +0500

    Update problem03.c

--- problem03.c
+++ problem03.c
@@ -1,1 +1,34 @@
+/* Q:03
+Write a program that takes three numbers and finds the largest number using nested if.
+*/
 
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2, num3;
+
+    printf("Enter number one: ");
+    scanf("%d", &num1); 
+    printf("Enter number two: ");
+    scanf("%d", &num2);
+    printf("Enter number three: ");
+    scanf("%d", &num3);
+
+    
+    if (num1>num2) {
+        if (num1>num3)
+            printf("Num1 is largest\n");
+        else
+            printf("Num3 is largest\n");   
+    }
+    
+    else {
+        if (num2>num3)
+            printf("Num2 is largest\n");
+        else
+            printf("Num3 is largest\n");    
+    }
+        
+
+}
