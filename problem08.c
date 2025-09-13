728d154aefec1f7d374b645f5ec10be77c78d499
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:08:57 +0500

    Update problem08.c

--- problem08.c
+++ problem08.c
@@ -1,1 +1,20 @@
+/* Q:08
+If the temperature is greater than 30°C, display "Hot Day", otherwise "Pleasant Day".
+*/
 
+#include <stdio.h>
+
+int main(void)
+{
+    int temp;
+    printf("Enter the temp: ");
+    scanf("%d", &temp);
+
+    if (temp > 30)
+        printf("Hot day!\n");
+    else 
+        printf("What a pleasent day!\n");
+
+    return 0;
+
+}
