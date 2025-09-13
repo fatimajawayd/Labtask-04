7d572248566fea37564c4a5d74e22d71345552c5
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:09:35 +0500

    Update problem09.c

--- problem09.c
+++ problem09.c
@@ -1,1 +1,22 @@
+/* Q:09
+A student can only appear in the final exam if: Attendance ≥ 75% AND Internal marks ≥ 40.
+*/
 
+#include <stdio.h>
+
+int main(void)
+{
+    int attendance;
+    int internalmarks;
+     
+    printf("Enter your attendance: ");
+    scanf("%d", &attendance);
+    
+    printf("Enter your internal marks: ");
+    scanf("%d", &internalmarks);
+
+    if (attendance >= 75 && internalmarks >= 40)
+        printf("you can appear in the exam\n");
+    else
+        printf("You cannot appear in the exam\n");
+}
