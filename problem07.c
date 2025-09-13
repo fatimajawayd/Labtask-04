bd0031f9c3a84b3eabfa7e4081c0c4e4856cca99
Fatima Javed <k250594@nu.edu.pk>
2025-09-13 16:08:18 +0500

    Update problem07.c

--- problem07.c
+++ problem07.c
@@ -1,1 +1,20 @@
+/* Q:07
+A password checking system: If user enters "1234" then print access granted else print access denied.
+*/
 
+#include <stdio.h>
+
+int main(void)
+{
+    int password;
+
+    printf("Enter user's password: ");
+    scanf("%d", &password);
+
+    if( password == 1234)
+        printf("Access granted\n");
+    else
+        printf("Access denied\n");   
+        
+    return 0;    
+}
